fcf5fb546f9f327f514e2760b204c728ae4a2ccd
Proyectos-Hugo <proyectos.hugocambero@gmail.com>
2025-05-26 11:07:44 +0200

    update

--- 04_servicios_paises/src/controller/pais.controller.ts
+++ 04_servicios_paises/src/controller/pais.controller.ts
@@ -7,6 +7,7 @@ import {
 
 } from '@nestjs/common';
 import { PaisesService } from '../service/paises.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('pais')
 export class PaisController {
@@ -17,6 +18,8 @@ export class PaisController {
     return this.paisService.findAllContinentes();
   }
 
+  @ApiOperation({summary:"lista de continentes",description:"a partir del nombre del continente, devuelve la lista de paises"})
+  
   @Get('paisesContinente/:continente')
   paisesContinente(@Param("continente")continente:string){
     return this.paisService.findByContinente(continente);

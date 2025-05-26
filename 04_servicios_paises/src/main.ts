d85d1426274370e5e56a04ba363392086992e6e4
Proyectos-Hugo <proyectos.hugocambero@gmail.com>
2025-05-26 11:06:46 +0200

    update

--- 04_servicios_paises/src/main.ts
+++ 04_servicios_paises/src/main.ts
@@ -6,7 +6,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
   //configuración swagger
   const config = new DocumentBuilder()
     .setTitle('API de de paises')
@@ -16,6 +15,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('ayuda/api', app, document);
+  await app.listen(process.env.PORT ?? 3000);
+  
 }
 bootstrap();
 
